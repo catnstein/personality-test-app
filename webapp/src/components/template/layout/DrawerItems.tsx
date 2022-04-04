b7d954eb7b4cc5e4ab1f8eac1e367e1b6b119617
Catnstein <mircea.badragan@monsterlynx.com>
2022-04-04 10:53:32 +0300

    add route redirects in drawer items

--- webapp/src/components/template/layout/DrawerItems.tsx
+++ webapp/src/components/template/layout/DrawerItems.tsx
@@ -8,24 +8,32 @@ import {
 } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 import QuizIcon from "@mui/icons-material/Quiz";
+import { Link } from "react-router-dom";
+import { Paths } from "../../routes/paths.enum";
+
+const linkStyle = { textDecoration: "none", color: "black" };
 
 export const drawer = (
   <div>
     <Toolbar />
     <Divider />
     <List>
-      <ListItem button>
-        <ListItemIcon>
-          <HomeIcon />
-        </ListItemIcon>
-        <ListItemText primary={"Home"} />
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon>
-          <QuizIcon />
-        </ListItemIcon>
-        <ListItemText primary={"Quiz"} />
-      </ListItem>
+      <Link to={Paths.Home} style={{ ...linkStyle }}>
+        <ListItem button>
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Home"} />
+        </ListItem>
+      </Link>
+      <Link to={Paths.Quiz} style={{ ...linkStyle }}>
+        <ListItem button>
+          <ListItemIcon>
+            <QuizIcon />
+          </ListItemIcon>
+          <ListItemText primary={"Quiz"} />
+        </ListItem>
+      </Link>
     </List>
   </div>
 );
